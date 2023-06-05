5470ae8b3cd0f381b8b4de6015365a9e5c8c9d7f
BhavanaVanga <130207150+BhavanaVanga@users.noreply.github.com>
2023-06-05 21:31:37 +0530

    changing pic

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hii")
+var data1={image : "me.2.jpg",name : "V BHAVANA",designation:"intern"};
+var data2={image : "me.5.jpg",name : "Bhavya",designation:"student"};
+var isdata1=true;
+var displaydata;
+var changeMyPic=function(){
+    if(isdata1)
+    {
+        displaydata=data2;
+        isdata1=false;
+    }
+    else
+    {
+        displaydata=data1;
+        isdata2=false;
+    }
+}
