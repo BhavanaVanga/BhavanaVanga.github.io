7a30bdb4e02bf59cef059408876cdcb509280de0
BhavanaVanga <130207150+BhavanaVanga@users.noreply.github.com>
2023-06-06 21:45:12 +0530

    added script2.js

--- script2.js
+++ script2.js
@@ -3,6 +3,9 @@ var changemypic=function(){
  fetch("https://randomuser.me/api").then((response)=>response.json()).then((data)=>{
     userinfo.name=data.results[0].name.first;
     userinfo.image=data.results[0].picture;
-    userinfo.username=data.results[0].username.first;
- })
+    userinfo.username=data.results[0].login.username;
+    document.getElementById("myimage").src=userinfo.image;
+    document.getElementById("myname").innerHTML=userinfo.name;
+    document.getElementById("mydesig").innerHTML=userinfo.username;
+ });
 }
